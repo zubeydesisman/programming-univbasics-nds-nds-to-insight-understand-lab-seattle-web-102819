a5b29a8b1be86b38f1f3a4582e690f0b200b1a25
Kellye Greene <kellyecodes@gmail.com>
2019-09-14 14:43:20 -0400

    Adds tests

--- spec/nds_explore_spec.rb
+++ spec/nds_explore_spec.rb
@@ -1,37 +1,13 @@
 require 'spec_helper'
+require 'pp'
 
-describe 'These examples' do
-  it 'can be used to confirm if a method is present in required relative' do
-    expect { test_method }.not_to raise_error
+describe 'Directors Database' do
+  it 'Pretty prints directors_database' do
+    expect { pretty_print_nds }.not_to output(directors_database).to_stdout
+    expect { pretty_print_nds }.to output(pretty_print_nds).to_stdout
   end
 
-  it 'can be used to confirm if a method is not present in required relative' do
-    expect { second_test_method }.to raise_error(NameError)
-  end
-
-  it 'can be used to confirm if a method returns expected results in required relative' do
-    expect(test_method).to eq('bears') # equal(): strict comparison, eq(): value comparison
-  end
-
-  it 'can be used to confirm if a class contains a specific method' do
-    expect(ExampleClass).to respond_to(:class_test)
-  end
-
-  it 'can be used to confirm if an instance method returns the expected results' do
-    example = ExampleClass.new
-    expect(example.instance_test).to eq('seriously')
-  end
-
-  it 'can be used to match strings' do
-    expect('There are two bears in a tree').to match(/bears in a tree/)
-    expect("They're both on the same limb").to include('limb')
-  end
-
-  it 'can check types' do
-    example = ExampleClass.new
-    expect(example).to be_instance_of(ExampleClass)
-
-    example_array = [1, 2, 3]
-    expect(example_array).to be_kind_of(Array)
+  it 'Iterates over Spielberg’s movies and prints out their names' do
+    expect { print_first_directores_movie_title }.to output("\"Jaws\"\n\"Close Encounters of the Third Kind\"\n\"Raiders of the Lost Ark\"\n\"E.T. the Extra-terrestrial\"\n\"Schindler's List\"\n\"Lincoln\"\n").to_stdout
   end
 end
