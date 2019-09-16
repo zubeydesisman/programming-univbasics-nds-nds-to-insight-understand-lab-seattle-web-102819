f23e721fbb4505e9764a3d602cfc8ae2deec1a66
Kellye Greene <kellyecodes@gmail.com>
2019-09-16 11:11:20 -0400

    Fix tests/typos

--- spec/nds_explore_spec.rb
+++ spec/nds_explore_spec.rb
@@ -4,10 +4,9 @@ require 'pp'
 describe 'Directors Database' do
   it 'Pretty prints directors_database' do
     expect { pretty_print_nds }.not_to output(directors_database).to_stdout
-    expect { pretty_print_nds }.to output(pretty_print_nds).to_stdout
   end
 
   it 'Iterates over Spielberg’s movies and prints out their names' do
-    expect { print_first_directores_movie_title }.to output("\"Jaws\"\n\"Close Encounters of the Third Kind\"\n\"Raiders of the Lost Ark\"\n\"E.T. the Extra-terrestrial\"\n\"Schindler's List\"\n\"Lincoln\"\n").to_stdout
+    expect { print_first_directors_movie_title }.to output("Jaws\nClose Encounters of the Third Kind\nRaiders of the Lost Ark\nE.T. the Extra-terrestrial\nSchindler's List\nLincoln\n").to_stdout
   end
 end
